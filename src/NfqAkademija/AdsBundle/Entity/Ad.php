b535733dac7b10f000dba14594977061abc2f4ac
Žilvinas Gadliauskas <zilvinas@gadliauskas.lt>
2014-04-15 11:13:46 +0300

    Added entities for Ads and Campgains

---
+++ src/NfqAkademija/AdsBundle/Entity/Ad.php
@@ -0,0 +1,241 @@
+<?php
+
+namespace NfqAkademija\AdsBundle\Entity;
+
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * NfqAkademija\AdsBundle\Entity\Ad
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="NfqAkademija\AdsBundle\Entity\AdRepository")
+ * @UniqueEntity("googleId")
+ */
+class Ad
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var text $campgainId
+     *
+     * @ORM\ManyToOne(targetEntity="NfqAkademija\AdsBundle\Entity\Campgain", inversedBy="ads")
+     * @ORM\JoinColumn(name="campgain_id", referencedColumnName="id")
+     */
+    private $campgainId;
+
+    /**
+     * @var text $name
+     * 
+     * @ORM\Column(name="name", type="text")
+     */
+    private $name;
+
+    /**
+     * @var text $description
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var text $imageUrl
+     *
+     * @ORM\Column(name="image_url", type="text")
+     */
+    private $imageUrl;
+
+    /**
+     * @var text $url
+     *
+     * @ORM\Column(name="url", type="text")
+     */
+    private $url;
+
+    /**
+     * @var text $googleId
+     *
+     * @ORM\Column(name="google_id", type="text")
+     */
+    private $googleId;
+
+    /**
+     * @var text $keyword
+     *
+     * @ORM\Column(name="keyword", type="text")
+     */
+    private $keyword;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set campgainId
+     *
+     * @param text $campgainId
+     * @return Ad
+     */
+    public function setCampgainId($campgainId)
+    {
+        $this->campgainId = $campgainId;
+        return $this;
+    }
+
+    /**
+     * Get campgainId
+     *
+     * @return text 
+     */
+    public function getCampgainId()
+    {
+        return $this->campgainId;
+    }
+
+    /**
+     * Set name
+     *
+     * @param text $name
+     * @return Ad
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return text 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param text $description
+     * @return Ad
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return text 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set imageUrl
+     *
+     * @param text $imageUrl
+     * @return Ad
+     */
+    public function setImageUrl($imageUrl)
+    {
+        $this->imageUrl = $imageUrl;
+        return $this;
+    }
+
+    /**
+     * Get imageUrl
+     *
+     * @return text 
+     */
+    public function getImageUrl()
+    {
+        return $this->imageUrl;
+    }
+
+    /**
+     * Set url
+     *
+     * @param text $url
+     * @return Ad
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return text 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set googleId
+     *
+     * @param text $googleId
+     * @return Ad
+     */
+    public function setGoogleId($googleId)
+    {
+        $this->googleId = $googleId;
+        return $this;
+    }
+
+    /**
+     * Get googleId
+     *
+     * @return text 
+     */
+    public function getGoogleId()
+    {
+        return $this->googleId;
+    }
+
+    /**
+     * Set keyword
+     *
+     * @param text $keyword
+     * @return Ad
+     */
+    public function setKeyword($keyword)
+    {
+        $this->keyword = $keyword;
+        return $this;
+    }
+
+    /**
+     * Get keyword
+     *
+     * @return text 
+     */
+    public function getKeyword()
+    {
+        return $this->keyword;
+    }
+}
---
+++ src/NfqAkademija/AdsBundle/Entity/Campgain.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace NfqAkademija\AdsBundle\Entity;
+
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * NfqAkademija\AdsBundle\Entity\Campgain
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="NfqAkademija\AdsBundle\Entity\CampgainRepository")
+ * @UniqueEntity("googleId")
+ */
+class Campgain
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer $userId
+     *
+     * @ORM\Column(name="user_id", type="integer")
+     */
+    private $userId;
+
+    /**
+     * @var text $googleId
+     * @ORM\Column(name="google_id", type="text")
+     */
+    private $googleId;
+
+    /**
+     * @var text $name
+     *
+     * @ORM\Column(name="name", type="text")
+     */
+    private $name;
+
+    /**
+     * @var float $budget
+     *
+     * @ORM\Column(name="budget", type="float")
+     */
+    private $budget;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="NfqAkademija\AdsBundle\Entity\Ad", mappedBy="campgainId")
+     */
+    private $ads;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     * @return Campgain
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return integer 
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set googleId
+     *
+     * @param text $googleId
+     * @return Campgain
+     */
+    public function setGoogleId($googleId)
+    {
+        $this->googleId = $googleId;
+        return $this;
+    }
+
+    /**
+     * Get googleId
+     *
+     * @return text 
+     */
+    public function getGoogleId()
+    {
+        return $this->googleId;
+    }
+
+    /**
+     * Set name
+     *
+     * @param text $name
+     * @return Campgain
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return text 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set budget
+     *
+     * @param float $budget
+     * @return Campgain
+     */
+    public function setBudget($budget)
+    {
+        $this->budget = $budget;
+        return $this;
+    }
+
+    /**
+     * Get budget
+     *
+     * @return float 
+     */
+    public function getBudget()
+    {
+        return $this->budget;
+    }
+    public function __construct()
+    {
+        $this->ads = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * Add ads
+     *
+     * @param NfqAkademija\AdsBundle\Entity\Ad $ads
+     * @return Campgain
+     */
+    public function addAd(\NfqAkademija\AdsBundle\Entity\Ad $ads)
+    {
+        $this->ads[] = $ads;
+        return $this;
+    }
+
+    /**
+     * Remove ads
+     *
+     * @param NfqAkademija\AdsBundle\Entity\Ad $ads
+     */
+    public function removeAd(\NfqAkademija\AdsBundle\Entity\Ad $ads)
+    {
+        $this->ads->removeElement($ads);
+    }
+
+    /**
+     * Get ads
+     *
+     * @return Doctrine\Common\Collections\Collection 
+     */
+    public function getAds()
+    {
+        return $this->ads;
+    }
+}
